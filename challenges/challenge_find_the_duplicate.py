b41a15f3b529ce4775cb5c202284d9dc119113d8
Ruan Chagas <ruanchagas95@gmail.com>
2024-03-14 04:19:10 -0300

    req05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,7 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    obj = {}
+    for num in nums:
+        obj[num] = obj.get(num, 0) + 1
+        if (obj[num] == 2 and num > 0):
+            return num
+    return False
