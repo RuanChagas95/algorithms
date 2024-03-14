3c5c6883ae0d638d551f0f06446d39d80a1d82f0
Ruan Chagas <ruanchagas95@gmail.com>
2024-03-14 02:01:18 -0300

    req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if not (isinstance(target_time, int)):
+        return None
+    for start, end in permanence_period:
+        if not (isinstance(start, int)
+                and isinstance(end, int)):
+            return None
+        if (start <= target_time <= end):
+            count += 1
+    return count
