02b0848f4705c47f6afb1ef58848ac3df2a3c058
Ruan Chagas <ruanchagas95@gmail.com>
2024-03-14 02:43:38 -0300

    req02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    invalids = [("hello", None), (None, 3), (1, 1)]
+    for invalid in invalids:
+        with pytest.raises(Exception):
+            encrypt_message(*invalid)
+    assert encrypt_message("hello", 3) == "leh_ol"
+    assert encrypt_message("hello", 9) == "olleh"
+    assert encrypt_message("hello", 4) == "o_lleh"
+    assert encrypt_message("hello", -1) == "olleh"
